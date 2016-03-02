75328356bed74947e8b907a0183a515f12e3e0e6
Enot <Enot>
2016-03-02 23:47:20 +0400

    Initial commit

---
+++ app/src/main/java/com/example/student15/service_25_02_2016/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.student15.service_25_02_2016;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager manager;
+    Sensor lightSensor;
+    List sensorList;
+    String info;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView2);
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorList = manager.getSensorList(Sensor.TYPE_ALL);
+        info = new String();
+        int i = 0;
+        for (Object s : sensorList) {
+            Sensor tmp = (Sensor) s;
+            info += i++ + ")" + tmp.getName()+"\n";
+        }
+        text.setText(info);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
